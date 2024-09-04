9e8bc2bc2153779b4e5b3d389f32ef6fdee009bc
Roberto Contreras <contreras_sandoval@hotmail.com>
2024-09-04 12:53:21 -0400

    :tada:hito-3-proyecto0804

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import routes from "./routes/router.js";
+
+const app = express();
+const PORT =  process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {console.log(`Listening on port ${PORT}`);});
