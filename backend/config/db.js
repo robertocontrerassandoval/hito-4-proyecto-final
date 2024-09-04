c85a886a81b633e3aa801833c6d6dec901d63aa6
Roberto Contreras <contreras_sandoval@hotmail.com>
2024-09-04 18:48:21 -0400

    :tada:hito-3-proyecto0804-2

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,3 +13,14 @@ const config = {
     database:DB_SATABASE,
     allowExitOnIdle: true
 };
+
+try {
+    await pool.query('SELECT NOW()');
+    console.log('Base de datos conectada    ');
+} catch (error) {
+    
+}
+
+const pool = new Pool(config)
+
+export default pool
