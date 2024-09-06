855dd759e783f2b2ac6c1f18136a6704e9a9fbc2
Ruben Norambuena <rubenalejhandro.norambuena@gmail.com>
2024-09-05 23:54:29 -0400

    version-1.4-05-09

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,29 @@
 import pg from "pg";
 import 'dotenv/config';
-const {Pool} = pg; //este Pool es de pg
 
+const { Pool } = pg; // Pool es de pg
 
-const {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE  } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const config = {
-    host:DB_HOST,
-    port:DB_PORT,
-    user:DB_USER,
-    password:DB_PASSWORD,
-    database:DB_DATABASE,
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
     allowExitOnIdle: true
 };
 
-try {
-    await pool.query('SELECT NOW()');
-    console.log('🚨🚨 Base de datos conectada 🚨🚨');
-} catch (error) {
-    
-}
+const pool = new Pool(config); // Primero inicializamos el pool
 
-const pool = new Pool(config)  //este pool es el que conecta con la base de datos
+// Probar la conexión
+(async () => {
+    try {
+        await pool.query('SELECT NOW()');
+        console.log('🚨🚨 Base de datos conectada 🚨🚨');
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+    }
+})();
 
-export default pool
+export default pool;
