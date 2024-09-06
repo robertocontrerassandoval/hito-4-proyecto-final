33d2afdc3611f78de836f1fa8532a259b232b19c
Ruben Norambuena <rubenalejhandro.norambuena@gmail.com>
2024-09-06 01:30:49 -0400

    version-1.6-06-09

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import { productController } from '../controllers/productController.js';
+import { validateProductData, validateProductId } from './productMiddleware.js';
 import { verifyToken } from '../middlewares/userMiddleware.js';
 
 const router = express.Router();
 
 router.get('/products', productController.getProducts);
-
-router.get('/products/:id', productController.getProductById);
-
-router.post('/products', verifyToken, productController.createProduct);  // Ruta protegida
-
-router.put('/products/:id', verifyToken, productController.updateProduct);  // Ruta protegida
-
-router.delete('/products/:id', verifyToken, productController.deleteProduct);  // Ruta protegida
+router.get('/products/:id', validateProductId, productController.getProductById);
+router.post('/products', verifyToken, validateProductData, productController.createProduct);
+router.put('/products/:id', verifyToken, validateProductId, validateProductData, productController.updateProduct);
+router.delete('/products/:id', verifyToken, validateProductId, productController.deleteProduct);
 
 export default router;
+
