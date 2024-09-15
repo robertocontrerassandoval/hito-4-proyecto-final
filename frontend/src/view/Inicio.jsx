7f01019c07398b85c5ad2b6916cce987318098b7
Ruben Norambuena <rubenalejhandro.norambuena@gmail.com>
2024-09-15 03:14:31 -0300

    version-15-09-03:14

--- frontend/src/view/Inicio.jsx
+++ frontend/src/view/Inicio.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
-import NavbarInicio from '../components/NavbarInicio'
-import { Container,Row, Col, Form, Button} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react'
+// import NavbarInicio from '../components/NavbarInicio'
+// import { Container,Row, Col, Form, Button} from 'react-bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const Inicio = () => {
-  return (
+// const Inicio = () => {
+//   return (
 
-   <>
-   <Container className="container-inicio d-flex flex-column justify-content-space-around">
+//    <>
+//    <Container className="container-inicio d-flex flex-column justify-content-space-around">
      
      
-     <Container className='container-navbar-inicio'>
-          <NavbarInicio/>
-      </Container>
+//      <Container className='container-navbar-inicio'>
+//           <NavbarInicio/>
+//       </Container>
 
       
 
-          <Container >
-           pagina inicio
-          </Container>   
+//           <Container >
+//            pagina inicio
+//           </Container>   
 
-            </Container>
+//             </Container>
        
-   </>
+//    </>
 
     
   
-  )
-}
+//   )
+// }
 
-export default Inicio
+// export default Inicio
+
+import React from 'react';
+import NavbarInicio from '../components/NavbarInicio';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Inicio = () => {
+  return (
+    <>
+      <Container className="d-flex flex-column min-vh-100">
+        <Row>
+          <Col>
+            <NavbarInicio />
+          </Col>
+        </Row>
+        <Row className="flex-grow-1">
+          <Col className="d-flex align-items-center justify-content-center">
+            <h1>Página de Inicio</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Inicio;
