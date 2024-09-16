f0d36169d3ae22c27c4ccc59260a8f255aee3a0b
Roberto Contreras <contreras_sandoval@hotmail.com>
2024-09-16 17:00:41 -0300

    :tada:actualizacion 9

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,7 @@ const app = express();
 
 // Middleware
 // Configuración de CORS
-const corsOptions = {
-    origin: 'https://hito-4-proyecto-final.onrender.com', // Ajusta según el origen que necesites permitir
-};
 
-// Usa CORS con las opciones especificadas
-app.use(cors(corsOptions));
 
 app.use(express.json()); // Analizar el cuerpo de las solicitudes en formato JSON
 
