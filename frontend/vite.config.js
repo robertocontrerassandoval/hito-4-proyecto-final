e984c515bfc2065694455e08a3f426d26c114160
Roberto Contreras <contreras_sandoval@hotmail.com>
2024-09-16 16:24:45 -0300

    :tada:actualizacion 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
-export default {
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://hito-4-proyecto-final-1-ozdl.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
